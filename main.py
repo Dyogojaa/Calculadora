202862bee19c6543658ce9c343c2cecd32529535
Dyogo <dyogo@gmail.com>
2023-09-09 15:29:01 -0300

    Novos Botoes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from variables import WINDOW_ICON_PATH
 from display import Display
 from info import Info
 from styles import setupTheme
-from buttons import Button
+from buttons import ButtonsGrid
 
 if __name__ == '__main__':
 
@@ -21,19 +21,23 @@ if __name__ == '__main__':
 
     # Info
     info = Info('2.0 ^ 10.0 = 1024')
-    window.addToVLayout(info)
+    window.addWidgetToVLayout(info)
 
     # Display
     display = Display()
     # display.setPlaceholderText("Digite algo")
-    window.addToVLayout(display)
+    window.addWidgetToVLayout(display)
 
     # Buttons
-    button = Button('Texto do botão')
-    window.addToVLayout(button)
+    # button = Button('Texto do botão')
+    # window.addLayout(button)
 
-    button2 = Button('Texto do botão')
-    window.addToVLayout(button2)
+    # button2 = Button('Texto do botão')
+    # window.addLayout(button2)
+
+    # Grid
+    buttonsGrid = ButtonsGrid()
+    window.vLayout.addLayout(buttonsGrid)
 
     window.ajustFixedSize()
     window.show()
